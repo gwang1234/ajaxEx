acbcc6f2888830310df58ee093ebede93008b365
gwang1234 <qoi11@naver.com>
2024-03-14 20:19:55 +0900

    first

--- ajaxEx/src/main/java/com/ajaxEx/controller/HomeController.java
+++ ajaxEx/src/main/java/com/ajaxEx/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+//ere
     @GetMapping("/")
     public String index() {
         return "index";
